4887cd2b0025e2cad15c9bf5d0b04c57d327c5ff
sota <sota@gmail.com>
2020-03-11 15:33:06 +0900

    to merge

--- eval/eval_classifier_i2w.py
+++ eval/eval_classifier_i2w.py
@@ -18,13 +18,13 @@ parser.add_argument('--image_root', type=str,
 parser.add_argument('--pkl_path', type=str,
                     default='/mnt/fs2/2019/Takamuro/db/i2w/sepalated_data.pkl')
 parser.add_argument('--output_dir', type=str,
-                    default='/mnt/fs2/2019/takamuro/results/c-UNet/eval_classifier_i2w/i2w_strict_res101_train')
+                    default='/mnt/fs2/2019/takamuro/m2_research/weather_transfer/results/eval_classifier_i2w/i2w_strict_sep_res101_val')
 # imb mean imbalanced
 parser.add_argument('--classifer_path', type=str,
-                    default='/mnt/data2/takamuro/m2/cUNet-Pytorch/cp/classifier/classifier_i2w_strict/better_resnet101_10.pt')
+                    default='/mnt/fs2/2019/Takamuro/m2_research/weather_transfer/cp/classifier_i2w_for_val_strict_sep/resnet101_epoch15_step59312.pt')
 # parser.add_argument('--classifer_path', type=str, default='cp/classifier/res_aug_5_cls/resnet101_95.pt')
 parser.add_argument('--input_size', type=int, default=224)
-parser.add_argument('--batch_size', type=int, default=16)
+parser.add_argument('--batch_size', type=int, default=10)
 parser.add_argument('--num_workers', type=int, default=4)
 parser.add_argument('--num_classes', type=int, default=5)
 
@@ -36,7 +36,7 @@ import torchvision.transforms as transforms
 
 sys.path.append(os.getcwd())
 from dataset import ClassImageLoader
-from sampler import ImbalancedDatasetSampler
+# from sampler import ImbalancedDatasetSampler
 
 
 if __name__ == '__main__':
@@ -55,7 +55,7 @@ if __name__ == '__main__':
 
     loader = torch.utils.data.DataLoader(
             dataset,
-            sampler=ImbalancedDatasetSampler(dataset),
+            # sampler=ImbalancedDatasetSampler(dataset),
             batch_size=args.batch_size,
             drop_last=True,
             num_workers=args.num_workers)
